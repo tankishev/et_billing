10708286b967b64a2f86dcc0f7e1467269a5de87
Lyubomir Tankishev <tankishev@gmail.com>
2023-03-29 07:38:06 +0300

    initial commit billing

---
+++ Billing/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('admin/', admin.site.urls),
+    # path("accounts/", include("django.contrib.auth.urls")),
+    path("reports/", include('reports.urls')),
+    path("vendors/", include('vendors.urls')),
+    path("stats/", include('stats.urls')),
+    path("packages/", include('packages.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
