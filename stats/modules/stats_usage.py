3eb046b6a1b17bace72f490d54c8877952461218
Lyubomir Tankishev <tankishev@gmail.com>
2023-04-14 11:33:42 +0300

    updated save_service_usage parameters

--- stats/modules/stats_usage.py
+++ stats/modules/stats_usage.py
@@ -1,11 +1,10 @@
 from ..models import UsageStats
 
 
-def save_service_usage(data):
-    """ Saves service usage statistics
-    :param data: tuple in format ('2022-12', vendor_id, service_id, 123)
-    """
-    period, vendor_id, service_id, unit_count = data
+def save_service_usage(period: str, vendor_id: int, service_id: int, unit_count: int) -> None:
+    """ Saves or updates service usage statistics. """
+    # :param data: tuple in format ('2022-12', vendor_id, service_id, 123)
+    # period, vendor_id, service_id, unit_count = data
 
     try:
         usage = UsageStats.objects.get(period=period, vendor_id=vendor_id, service_id=service_id)
