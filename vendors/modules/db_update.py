5bf9901b49c00b4e13a9c0dc45f309cf9f95a3f7
Lyubomir Tankishev <tankishev@gmail.com>
2023-04-14 11:15:11 +0300

    added basic logging

--- vendors/modules/db_update.py
+++ vendors/modules/db_update.py
@@ -1,10 +1,13 @@
 from services.modules import FiltersMixin
 from shared.modules import DBProxy
 from stats.modules import save_service_usage
-
 from .input_files import InputFilesMixin
 from .service_usage import ServiceUsageMixin
 from ..models import Vendor, VendorService
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class DBUpdater(FiltersMixin, ServiceUsageMixin, InputFilesMixin):
@@ -16,25 +19,27 @@ class DBUpdater(FiltersMixin, ServiceUsageMixin, InputFilesMixin):
         """ Read vendor input file and calculate service usage """
 
         period, vendor_id = input_file.period, input_file.vendor_id
+        logger.debug(f"Starting usage calcs for vendor {vendor_id} for {period}.")
 
         # Load dataframe and convert all numbers
         df = self.load_data_for_service_usage(input_file.file.path, skip_status_five)
         if df is None:
-            print(f'{period}: {vendor_id} - No transactions')
+            logger.info(f'vendor_id: {vendor_id}, period_id: {period}, return: No transactions')
             return 3
 
         # Load service filters
         service_filters = self.get_service_filters()
         services = self.load_vendor_service_filters(vendor_id, service_filters)
         if services is None:
-            print(f'{period}: {vendor_id} - No services configured')
+            logger.info(f'vendor_id: {vendor_id}, period_id {period}, return: No services configured')
             return 1
 
         # Calculate transaction based services
+        logger.debug("Calculating transaction based stats")
         df, is_reconciled = self.calc_usage(df, services)
         self.update_vendor_is_reconciled(vendor_id, is_reconciled)
         if not is_reconciled:
-            print(f'{period}: {vendor_id} - Not reconciled')
+            logger.info(f'vendor_id: {vendor_id}, period_id: {period}, return : Not reconciled')
             return 4
 
         # Get transaction based stats
@@ -43,6 +48,7 @@ class DBUpdater(FiltersMixin, ServiceUsageMixin, InputFilesMixin):
 
         # Get aggregation based stats
         if data and "Bio required" in df.columns:
+            logger.debug("Calculating BioID stats")
             df_bio = df[df["Bio required"] == 'yes'][["ThreadID"]].copy()
             df_bio = df_bio.drop_duplicates()
             n = len(df_bio)
@@ -51,19 +57,26 @@ class DBUpdater(FiltersMixin, ServiceUsageMixin, InputFilesMixin):
 
         # Add unique users where enabled requiring enablement
         if VendorService.objects.filter(vendor_id=vendor_id, service_id=32).exists():
+            logger.debug("Calculating unique users stats")
             n = df["PID receiver"].dropna().nunique()
             if n > 0:
                 data.append((32, n))
 
         # Save usage stats
+        logger.debug("Saving usage stats")
         for el in data:
-            save_service_usage((period, vendor_id, *el))
-        print(f'{period}: {vendor_id} - Complete')
+            service_id, unit_count = el
+            save_service_usage(period, vendor_id, service_id, unit_count)
+        logger.info(f'vendor_id: {vendor_id}, period_id: {period}, return : Complete')
         return 0
 
     @staticmethod
     def update_vendor_is_reconciled(vendor_id, is_reconciled=False):
-        vendor = Vendor.objects.get(vendor_id=vendor_id)
-        if vendor.is_reconciled != is_reconciled:
-            vendor.is_reconciled = is_reconciled
-            vendor.save()
+        try:
+            vendor = Vendor.objects.get(vendor_id=vendor_id)
+            if vendor.is_reconciled != is_reconciled:
+                logger.debug(f"Updating vendor is_reconciled - set status {is_reconciled}")
+                vendor.is_reconciled = is_reconciled
+                vendor.save()
+        except Vendor.DoesNotExist:
+            logger.warning(f"No vendor object with vendor_id {vendor_id}")
--- vendors/modules/vendor_calc.py
+++ vendors/modules/vendor_calc.py
@@ -1,26 +1,36 @@
 from datetime import datetime as dt
 from vendors.models import VendorInputFile
 from .db_update import DBUpdater
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def recalc_vendor(period, vendor_id):
+    logger.info(f"Starting usage calcs for vendor {vendor_id} for {period}.")
     start = dt.now()
     try:
         input_file = VendorInputFile.objects.get(period=period, vendor_id=vendor_id, is_active=True)
+        logger.debug("Input file loaded")
         dbu = DBUpdater()
         res = dbu.save_service_usage_period_vendor(input_file)
+        logger.debug(f"Calc result: {res}")
         return res, vendor_id
     except VendorInputFile.DoesNotExist:
+        logger.warning(f"No usage file for vendor {vendor_id} in {period}.")
         return 2, vendor_id
     finally:
-        print(dt.now() - start)
+        execution_time = dt.now - start
+        logger.info(f'Execution time: {execution_time}')
 
 
 def recalc_all_vendors(period):
+    logger.info(f"Starting usage calcs for ALL vendors for {period}.")
     start = dt.now()
     output = dict()
     try:
         input_files = VendorInputFile.objects.filter(period=period, is_active=True).order_by('vendor_id')
+        logger.debug(f'{len(input_files)} input files loaded')
         dbu = DBUpdater()
         for input_file in input_files:
             res = dbu.save_service_usage_period_vendor(input_file)
@@ -29,6 +39,8 @@ def recalc_all_vendors(period):
             output[res].append(input_file.vendor_id)
         return output
     except VendorInputFile.DoesNotExist:
+        logger.warning("No input files found")
         return output
     finally:
-        print(dt.now() - start)
+        execution_time = dt.now() - start
+        logger.info(f'Execution time: {execution_time}')
