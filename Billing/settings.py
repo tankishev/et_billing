d58cbd0de1b77a80d02295cd9a1c123a257dd025
Lyubomir Tankishev <tankishev@gmail.com>
2023-05-09 19:16:12 +0300

    removed secret key from production

--- Billing/settings.py
+++ Billing/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wlr9g6ra49ly)6h(6n3*_^x$a7=sx=y0uyo2+9ozo9jh-$bm*4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
