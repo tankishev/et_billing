b4610384ba517ebcb915eb7ba63effee13e5d797
tankishev <98879302+tankishev@users.noreply.github.com>
2023-12-05 08:42:19 +0200

    update settings.py

--- Billing/settings.py
+++ Billing/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DJANGO_DEBUG').lower() == 'true')
 
-ALLOWED_HOSTS = ['127.0.0.1', '85.196.148.19']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
